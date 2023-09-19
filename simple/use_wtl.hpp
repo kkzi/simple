00454f3df4f34b31034e7dbe3a895b50c96719aa
gx <xuan.guo@atomdatatech.com>
2023-09-19 11:38:22 +0800

    update use_wtl

--- simple/use_wtl.hpp
+++ simple/use_wtl.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+#if defined _M_IX86
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
 #include <atldef.h>
+#include <atlbase.h>
 #include <atlapp.h>
 #include <atlframe.h>
 #include <atlctrls.h>
@@ -9,21 +20,23 @@
 #include <atltheme.h>
 #include <concepts>
 
+
+extern CAppModule _Module;
+
 namespace WTL
 {
     template<class T> concept CanCreate = requires(T obj) { { obj.Create(NULL) } -> std::convertible_to<HWND>; };
     template<class T> concept CanCreateEx = requires(T obj) { { obj.CreateEx(NULL) }-> std::convertible_to<HWND>; };
-    template<CanCreate T> HWND Create(T& t) { return t.Create(NULL); }
-    template<CanCreateEx T> HWND Create(T& t) { return t.CreateEx(NULL); }
 
-    template<class Win>
-    static int Run(HINSTANCE hInstance, LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
+
+    template<class Win, class... Args>
+    static int Run(HINSTANCE hInstance, int cmdshow, Args &&...args)
     {
         HRESULT res = ::CoInitialize(NULL);
         ATLASSERT(SUCCEEDED(res));
 
         ::DefWindowProc(NULL, 0, 0, 0L);
-        WTL::AtlInitCommonControls(ICC_COOL_CLASSES | ICC_BAR_CLASSES);
+        AtlInitCommonControls(ICC_COOL_CLASSES | ICC_BAR_CLASSES | ICC_USEREX_CLASSES);
 
         res = _Module.Init(NULL, hInstance);
         ATLASSERT(SUCCEEDED(res));
@@ -31,13 +44,20 @@ namespace WTL
         CMessageLoop loop;
         _Module.AddMessageLoop(&loop);
 
-        Win win;
-        if (Create(win) == NULL)
+        Win win(std::forward<Args>(args)...);
+        HWND hwnd = 0;
+        if constexpr (CanCreateEx<Win>) {
+            hwnd = win.CreateEx(NULL);
+        }
+        else if constexpr (CanCreate<Win>) {
+            hwnd = win.Create(NULL);
+        }
+        if (hwnd == NULL)
         {
             ATLTRACE(_T("Main window creation failed!\n"));
             return 0;
         }
-        win.ShowWindow(nCmdShow);
+        win.ShowWindow(cmdshow);
 
         int ret = loop.Run();
         _Module.RemoveMessageLoop();
